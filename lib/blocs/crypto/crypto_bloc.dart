24dd7ecc663f06016af49bbd706ea0db8900a400
MarcusNg <MarcusNg@users.noreply.github.com>
2021-04-20 14:01:39 -0400

    add crypto bloc

--- lib/blocs/crypto/crypto_bloc.dart
+++ lib/blocs/crypto/crypto_bloc.dart
@@ -4,17 +4,39 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc_crypto/models/coin_model.dart';
 import 'package:flutter_bloc_crypto/models/failure_model.dart';
+import 'package:flutter_bloc_crypto/repositories/crypto_repository.dart';
 
 part 'crypto_event.dart';
 part 'crypto_state.dart';
 
 class CryptoBloc extends Bloc<CryptoEvent, CryptoState> {
-  CryptoBloc() : super(CryptoState.initial());
+  final CryptoRepository _cryptoRepository;
+
+  CryptoBloc({required CryptoRepository cryptoRepository})
+      : _cryptoRepository = cryptoRepository,
+        super(CryptoState.initial());
 
   @override
   Stream<CryptoState> mapEventToState(
     CryptoEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is AppStarted) {
+      yield* _mapAppStartedToState();
+    } else if (event is RefreshCoins) {
+    } else if (event is LoadMoreCoins) {}
+  }
+
+  Stream<CryptoState> _mapAppStartedToState() async* {
+    yield state.copyWith(status: CryptoStatus.loading);
+    // Request Coins
+    try {
+      final coins = await _cryptoRepository.getTopCoins();
+      yield state.copyWith(coins: coins, status: CryptoStatus.loaded);
+    } on Failure catch (err) {
+      yield state.copyWith(
+        failure: err,
+        status: CryptoStatus.error,
+      );
+    }
   }
 }

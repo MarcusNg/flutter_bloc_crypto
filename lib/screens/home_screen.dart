428abede3f4e07d6506ea42e8def93a44012149f
MarcusNg <MarcusNg@users.noreply.github.com>
2021-04-20 11:53:30 -0400

    add app bar and gradient background

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Top Coins'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme.of(context).primaryColor,
+              Colors.grey[900]!,
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

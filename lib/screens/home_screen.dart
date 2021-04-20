e140dcefe7dc604a0109e2daacec64325822846f
MarcusNg <MarcusNg@users.noreply.github.com>
2021-04-20 13:03:13 -0400

    add future builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_crypto/models/coin_model.dart';
+import 'package:flutter_bloc_crypto/repositories/crypto_repository.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -23,37 +25,50 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
-        child: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              leading: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '${++index}',
+        child: FutureBuilder(
+          future: CryptoRepository().getTopCoins(),
+          builder: (BuildContext context, AsyncSnapshot<List<Coin>> snapshot) {
+            if (!snapshot.hasData)
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor:
+                      AlwaysStoppedAnimation(Theme.of(context).accentColor),
+                ),
+              );
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) {
+                final coin = snapshot.data![index];
+                return ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${++index}',
+                        style: TextStyle(
+                          color: Theme.of(context).accentColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  title: Text(
+                    coin.fullName,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  subtitle: Text(
+                    coin.name,
+                    style: const TextStyle(color: Colors.white70),
+                  ),
+                  trailing: Text(
+                    '\$${coin.price.toStringAsFixed(4)}',
                     style: TextStyle(
                       color: Theme.of(context).accentColor,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
-                ],
-              ),
-              title: Text(
-                'Bitcoin',
-                style: const TextStyle(color: Colors.white),
-              ),
-              subtitle: Text(
-                'BTC',
-                style: const TextStyle(color: Colors.white70),
-              ),
-              trailing: Text(
-                '\$50000.0000',
-                style: TextStyle(
-                  color: Theme.of(context).accentColor,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
+                );
+              },
             );
           },
         ),
